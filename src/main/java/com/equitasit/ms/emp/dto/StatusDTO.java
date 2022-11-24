8c559c363855a1680c8882c6f513f2e14d5300c1
srini-equitasit <srini@equitasitinc.com>
2022-11-24 19:09:08 +0530

    removed unused code

--- src/main/java/com/equitasit/ms/emp/dto/StatusDTO.java
+++ src/main/java/com/equitasit/ms/emp/dto/StatusDTO.java
@@ -1,21 +1,15 @@
 package com.equitasit.ms.emp.dto;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class StatusDTO {
-
-	public StatusDTO() {
-
-	}
-
-	public StatusDTO(String statusMsg) {
-		this.statusMsg = statusMsg;
-	}
-
-	private String statusMsg;
+    private String statusMsg;
+    
+    public StatusDTO(String statusMsg) {
+        this.statusMsg = statusMsg;
+    }
 
 }
