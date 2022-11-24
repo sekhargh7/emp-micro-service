d591be65d1577501feac57792de928056379d019
srini-equitasit <srini@equitasitinc.com>
2022-11-24 21:10:52 +0530

    Update EmpMicroServiceApplicationTests.java

--- src/test/java/com/equitasit/ms/emp/EmpMicroServiceApplicationTests.java
+++ src/test/java/com/equitasit/ms/emp/EmpMicroServiceApplicationTests.java
@@ -1,9 +1,12 @@
 package com.equitasit.ms.emp;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
@@ -11,8 +14,12 @@ import org.springframework.test.context.ActiveProfiles;
 //@EnableAutoConfiguration(exclude = SecurityAutoConfiguration.class)
 class EmpMicroServiceApplicationTests {
 
+	@Autowired
+	private ApplicationContext context;
+
 	@Test
 	void contextLoads() {
+		Assertions.assertNotNull(context);
 	}
 
 }
